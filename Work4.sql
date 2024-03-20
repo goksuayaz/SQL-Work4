82685f3b9e3084596352459817e52db56eda7490
Göksu Ayaz <goksuayaz79@gmail.com>
2024-03-21 02:32:18 +0300

    SQL-Work4

---
+++ Work4.sql
@@ -0,0 +1,6 @@
+--SELECT DISTINCT replacement_cost FROM film;
+--SELECT COUNT(DISTINCT replacement_cost) AS unique_replacement_cost FROM film;
+--SELECT COUNT (*) AS  count_t_start_with_g FROM film WHERE title LIKE 'T%' AND rating = 'G';
+
+--SELECT COUNT(*) AS countries_with_5 FROM country WHERE LENGTH(country) = 5;
+--SELECT COUNT(*) AS cities_end_with_r FROM city WHERE city LIKE '%r';
